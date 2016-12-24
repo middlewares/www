956d58882b9e96fb7ba21aaa8f5fa4bf300aca6b
oscarotero <oom@oscarotero.com>
2016-12-24 10:42:52 +0100

    Updated http-middleware interface to 0.4 and php-cs-fixer to 2.0

--- src/Www.php
+++ src/Www.php
@@ -4,10 +4,10 @@ namespace Middlewares;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Interop\Http\Middleware\ServerMiddlewareInterface;
-use Interop\Http\Middleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
 
-class Www implements ServerMiddlewareInterface
+class Www implements MiddlewareInterface
 {
     /**
      * @var bool Add or remove www
